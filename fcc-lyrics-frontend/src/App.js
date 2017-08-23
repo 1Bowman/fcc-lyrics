370db03cc8bbfd4519ef528d078abd962cc96294
Alex Bowman <onlyonebowman@gmail.com>
2017-08-23 17:10:46 -0500

    Started setting up API endpoint for the front end

--- fcc-lyrics-frontend/src/App.js
+++ fcc-lyrics-frontend/src/App.js
@@ -1,18 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Button, FormGroup, FormControl } from 'react-bootstrap'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      'artist': '',
+      'song': '',
+      'response': {},
+    }
+  }
+
+  async componentDidMount(){
+    try{
+        let res = await fetch('http://localhost:5000/api/v1/lmfao/shots')
+        console.log(res.json())
+        // this.setState({
+        //   'response': res.value
+        // })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Lyrics Search</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <form>
+            <FormGroup>
+              <FormControl type="text" placeholder="Artist"></FormControl>
+            </FormGroup>
+
+            <FormGroup>
+              <FormControl type="text" placeholder="Song Title"></FormControl>
+            </FormGroup>
+
+            <FormGroup>
+              <Button bsStyle="primary" onClick={() => console.log('clicked')}>Search</Button>
+            </FormGroup>
+          </form>
+        </div>
       </div>
     );
   }
