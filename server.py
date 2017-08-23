e6288521985a430b1197e4c9cbd61be9e11406be
Alex Bowman <onlyonebowman@gmail.com>
2017-08-23 14:48:08 -0500

    Changed content to application/json

--- server.py
+++ server.py
@@ -5,5 +5,4 @@ app = Flask(__name__)
 @app.route('/api/v1/<artist>/<song_title>', methods=['GET'])
 def get_lyrics_and_swears(artist, song_title):
     ret_obj = generating(artist, song_title)
-    print(type(ret_obj))
-    return json.dumps(ret_obj)
+    return json.jsonify(ret_obj)
