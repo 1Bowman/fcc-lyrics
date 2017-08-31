88264e764e67ab94a5aef3c1ddac939c5ad913e7
Alex Bowman <onlyonebowman@gmail.com>
2017-08-30 19:00:36 -0500

    pre-heroku push

--- fcc-lyrics-frontend/src/SwearList.jsx
+++ fcc-lyrics-frontend/src/SwearList.jsx
@@ -1,41 +1,29 @@
 import React, { Component } from 'react'
+import { ListGroup, Badge } from 'react-bootstrap'
+import startCase from 'lodash/startCase'
 
 class SwearList extends Component {
-  constructor(props){
-    super(props)
-  }
 
-  renderSwears(){
-    if(this.state.error){
-      return <h2>404</h2>
-    }
-    else if(this.state.response.swears){
-      const swears = Object.entries(this.state.response.swears)
-
-      return swears.map(([key, value]) => {
-        return <p key={key}>{key} - {value}</p>
-      })
-    }
-  }
-
-  ren(){
+  renderListItem(){
     if(this.props.error){
-      return <h2>404 Invalid song/artist</h2>
+      return <h2>404 - Invalid song/artist</h2>
     }
     if(this.props.swears){
       const swears = Object.entries(this.props.swears)
       return swears.map(([key, value]) => {
-        return <p key={key}>{key} - {value}</p>
+        return <li  className="list-group-item"
+                    key={key}>
+                    <p>{ startCase(key) } <Badge pullRight={true}>{value}</Badge></p>
+              </li>
       })
     }
   }
 
   render() {
-    console.log('props', this.props)
     return (
-      <div>
-        { this.ren() }
-      </div>
+      <ListGroup componentClass="ul">
+        { this.renderListItem() }
+      </ListGroup>
     )
   }
 }
